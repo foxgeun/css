46fae50561fff2cc6d29968e5f13e13bad12a5d2
foxgeun <121280503+foxgeun@users.noreply.github.com>
2023-04-10 20:51:51 +0900

    Summary

---
+++ .vscode/jstest/js/clock.js
@@ -0,0 +1,13 @@
+const clock = document.querySelector("h2#clock");
+
+function sayHello(){
+    console.log("hello");
+}
+
+
+
+const date = new Date();
+date.getDay();
+
+
+setTimeout(sayHello, 5000);
